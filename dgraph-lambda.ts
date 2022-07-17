e4606db4d122c983bf0d4b268f792a34a7248e37
nach <napat_pat3@hotmail.com>
2022-07-17 21:44:06 +0700

    feat(dgraph-lambda.ts): validation

--- dgraph-lambda.ts
+++ dgraph-lambda.ts
@@ -1,6 +1,12 @@
 // You can type/paste your script here
 
 async function newUserResolver({args, graphql}) {
+    let validationError = ""
+    if(!(/^([A-Za-z0-9._]{1,})$/.test(args.username))) validationError += "invalid username, "
+    if(!(/^([a-z0-9._]{10,})$/.test(args.password))) validationError += "invalid password, "
+    if(!(/^([a-z0-9._]{8,})$/.test(args.profile_image_hash))) validationError += "invalid profile_image_hash, "
+
+    if(validationError){
     const results = await graphql(`mutation customMutation {
       addUser(input: {username: "${args.username}", profile_image_hash: "${args.profile_image_hash}", password: "${args.password}", thirdParty: {domain: "api.lorem.space", path: "image/face"}, joined_date:"${(new Date).toISOString()}"}) {
         user {
@@ -12,6 +18,9 @@ async function newUserResolver({args, graphql}) {
     console.log("-----------------------------------");
 
     return results.data.addUser.user[0].id
+    }else{
+      throw `validation fail code : ${validationError}`
+    }
 }
 
 self.addGraphQLResolvers({
