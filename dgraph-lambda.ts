fc0124cb658b357c52a5ac8694aa69478601bdc9
nach <napat_pat3@hotmail.com>
2022-07-17 18:55:38 +0700

    feat(dagraph-lambda): new single user

--- dgraph-lambda.ts
+++ dgraph-lambda.ts
@@ -1,62 +1,20 @@
 // You can type/paste your script here
 
-async function newUserResolver({ args, graphql }) {
-  // lets give every new author a reputation of 3 by default
-  const results = await graphql(`mutation MyMutation {
-      addUser(input: {username: ${args.username}, profile_image_hash: "a", password: "aaaaaa", thirdParty: {domain: "a", path: "a"}}) {
+async function newUserResolver({args, graphql}) {
+    // lets give every new author a reputation of 3 by default
+    const results = await graphql(`mutation customMutation {
+      addUser(input: {username: "${args.username}", profile_image_hash: "${args.profile_image_hash}", password: "${args.password}", thirdParty: {domain: "api.lorem.space", path: "image/face"}, joined_date:"${(new Date).toISOString()}"}) {
         user {
           id
         }
       }
     }`)
-  console.log(JSON.stringify(results));
-  console.log("-----------------------------------");
+    console.log(JSON.stringify(results));
+    console.log("-----------------------------------");
 
-  return results
+    return results.data.addUser.user[0].id
 }
 
 self.addGraphQLResolvers({
   "Mutation.newUser": newUserResolver
 })
-
-// -----------------------------------------------------------
-
-async function addUserWebhook({ event, dql, graphql, authHeader }) {
-  // execute what you want on addition of an user
-  // maybe send a welcome mail to the user
-  const results = await graphql(`mutation addUserWebhookMutation {
-      addUser(input: ${event.add.input}) {
-        user {
-          id
-        }
-      }
-    }`)
-  console.log(JSON.stringify(event));
-  console.log(JSON.stringify(event.add.input));
-  console.log(JSON.stringify(results));
-
-  console.log("-----------------------------------");
-
-}
-
-async function updateUserWebhook({ event, dql, graphql, authHeader }) {
-  // execute what you want on updation of an user
-  // maybe send a mail to the user informing that few details have been updated
-
-}
-
-self.addWebHookResolvers({
-  "User.add": addUserWebhook,
-  "User.update": updateUserWebhook,
-})
-
-function addUserInit(useObj) {
-
-  return {
-    username: useObj.username,
-    profile_image_hash: useObj.profile_image_hash,
-    thirdParty: { domain: "api.lorem.space", path: "image/face" },
-    password: useObj.password,
-    joined_date: (new Date).toISOString()
-  }
-}
