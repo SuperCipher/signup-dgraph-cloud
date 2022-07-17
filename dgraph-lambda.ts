40e773914fec0ed67431c7887bee2a58b3d0e649
nach <napat_pat3@hotmail.com>
2022-07-17 19:41:53 +0700

    docs(dgraph-lambda): remove unuse comment

--- dgraph-lambda.ts
+++ dgraph-lambda.ts
@@ -1,7 +1,6 @@
 // You can type/paste your script here
 
 async function newUserResolver({args, graphql}) {
-    // lets give every new author a reputation of 3 by default
     const results = await graphql(`mutation customMutation {
       addUser(input: {username: "${args.username}", profile_image_hash: "${args.profile_image_hash}", password: "${args.password}", thirdParty: {domain: "api.lorem.space", path: "image/face"}, joined_date:"${(new Date).toISOString()}"}) {
         user {
